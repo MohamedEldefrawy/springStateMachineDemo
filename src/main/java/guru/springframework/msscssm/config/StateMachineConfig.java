2a7c80134878fb516dc4f6672bf118f8fe19588d
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-12-24 14:21:48 +0200

    Configure state transitions

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 @Configuration
 @EnableStateMachine
@@ -15,10 +16,19 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
   @SneakyThrows
   @Override
   public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) {
-    states.withStates().initial(PaymentState.NEW)
-        .states(EnumSet.allOf(PaymentState.class))
-        .end(PaymentState.AUTHORIZE)
+    states.withStates().initial(PaymentState.NEW) // Define Initial state
+        .states(EnumSet.allOf(PaymentState.class)) // Define states
+        .end(PaymentState.AUTHORIZE)  // Define Termination states
         .end(PaymentState.AUTHORIZE_ERROR)
         .end(PaymentState.PRE_AUTHORIZE_ERROR);
+  }
+
+  @SneakyThrows
+  @Override
+  public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) {
+    transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+        .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTHORIZE).event(PaymentEvent.PRE_AUTHORIZE_APPROVED)
+        .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTHORIZE_ERROR).event(PaymentEvent.AUTHORIZE_DECLINED);
+
   }
 }
