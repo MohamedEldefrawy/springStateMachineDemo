43c0c14f0fcc8c52d182e9fa076f15d71b2b551d
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-12-24 13:59:18 +0200

    Create PaymentRepository

--- src/main/java/guru/springframework/msscssm/domain/Payment.java
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Payment {
   @Id
+  @GeneratedValue
   private Long id;
   @Enumerated(EnumType.STRING)
   private PaymentState paymentState;
