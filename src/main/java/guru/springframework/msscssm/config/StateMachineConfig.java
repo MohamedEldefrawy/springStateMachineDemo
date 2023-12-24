727f891d2d1e409db9e38f1922cd0f9e407bc45e
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-12-24 14:49:23 +0200

    change state machine configuration

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -4,31 +4,35 @@ import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
 import java.util.EnumSet;
 import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.EnableStateMachine;
-import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
-import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
-import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.config.StateMachineBuilder;
 
 @Configuration
 @EnableStateMachine
-public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+public class StateMachineConfig {
+
   @SneakyThrows
-  @Override
-  public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) {
-    states.withStates().initial(PaymentState.NEW) // Define Initial state
+  @Bean
+  public StateMachine<PaymentState, PaymentEvent> stateMachine() {
+    StateMachineBuilder.Builder<PaymentState, PaymentEvent> builder = StateMachineBuilder.builder();
+
+    builder.configureStates().
+        withStates().initial(PaymentState.NEW) // Define Initial state
         .states(EnumSet.allOf(PaymentState.class)) // Define states
         .end(PaymentState.AUTHORIZE)  // Define Termination states
         .end(PaymentState.AUTHORIZE_ERROR)
         .end(PaymentState.PRE_AUTHORIZE_ERROR);
-  }
 
-  @SneakyThrows
-  @Override
-  public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) {
-    transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+    builder.configureTransitions()
+        .withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
         .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTHORIZE).event(PaymentEvent.PRE_AUTHORIZE_APPROVED)
         .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTHORIZE_ERROR).event(PaymentEvent.AUTHORIZE_DECLINED);
 
+    StateMachine<PaymentState, PaymentEvent> stateMachine = builder.build();
+
+    return stateMachine;
   }
 }
