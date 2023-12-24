c54878fd3283ae5d935e7a9aad749f346bacbc73
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-12-24 14:49:45 +0200

    add state machine test class

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+  @Autowired
+  private StateMachine<PaymentState, PaymentEvent> stateMachine;
+  @Autowired
+  private Logger logger;
+
+  @Test
+  void testNewStateMachine() {
+    stateMachine.start();
+    logger.info(stateMachine.getState().toString());
+    stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+    logger.info(stateMachine.getState().toString());
+    stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE_APPROVED);
+    logger.info(stateMachine.getState().toString());
+  }
+
+
+}
